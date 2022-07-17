66d884109c30f981278ef5ce7c6756bebf9aaba5
Lucas Pontes Arruda <paluke94@gmail.com>
2022-07-17 18:51:31 -0300

    [feat] add UpdatedDroneDto

---
+++ src/main/java/com/futuereh/dto/UpdatedDroneDto.java
@@ -0,0 +1,24 @@
+package com.futuereh.dto;
+
+public class UpdatedDroneDto {
+  private String name;
+  
+  private String droneStatus;
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getDroneStatus() {
+    return droneStatus;
+  }
+
+  public void setDroneStatus(String droneStatus) {
+    this.droneStatus = droneStatus;
+  }
+  
+}
