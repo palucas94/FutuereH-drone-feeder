890bc4a394a95f51a6a953c61acb8ff943505522
Lucas Pontes Arruda <paluke94@gmail.com>
2022-07-18 06:31:20 -0300

    [test] implement DroneTest

--- src/test/java/com/futuereh/DroneTest.java
+++ src/test/java/com/futuereh/DroneTest.java
@@ -1,6 +1,7 @@
 package com.futuereh;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -14,19 +15,75 @@ import io.restassured.http.ContentType;
 public class DroneTest {
 
   @Test
-  @DisplayName("1 - Testa se o drone é criado corretamente.")
-  public void create() {
-    DroneDto droneDto = new DroneDto();
+  @DisplayName("1 - Criar drone.")
+  public void deveCriarDrone() {
+    DroneDto droneDto1 = new DroneDto();
     
-    droneDto.setName("Drone 1");
-    droneDto.setDroneStatus("disponivel");
+    droneDto1.setName("Drone 1");
+    droneDto1.setDroneStatus("disponivel");
     
     given()
       .contentType(ContentType.JSON)
-      .body(droneDto)
+      .body(droneDto1)
       .when()
       .post("/drone")
       .then()
-        .statusCode(204);
+        .statusCode(201)
+        .body("name", is("Drone 1"))
+        .body("droneStatus", is("disponivel"));
+  }
+  
+  @Test
+  @DisplayName("2 - Listar drones")
+  public void deveListarDronesCriados() {
+    DroneDto droneDto2 = new DroneDto();
+    droneDto2.setName("Drone 2");
+    droneDto2.setDroneStatus("disponivel");
+    
+    DroneDto droneDto3 = new DroneDto();
+    droneDto3.setName("Drone 3");
+    droneDto3.setDroneStatus("disponivel");
+
+    given().contentType(ContentType.JSON).body(droneDto2).post("/drone");
+    given().contentType(ContentType.JSON).body(droneDto3).post("/drone");
+
+    given().when().get("/drone").then().statusCode(200).body("list.size()", is(3));
+  }
+  
+  @Test
+  @DisplayName("3 - Buscar drone por Id")
+  public void deveRetornarBuscaPorId() {
+    given()
+    .when().get("/drone/2")
+    .then().statusCode(200).body("name", is("Drone 1"));
+  }
+  
+  @Test
+  @DisplayName("4 - Atualizar drone")
+  public void deveAtualizarDrone() {
+    DroneDto droneDto = new DroneDto();
+    droneDto.setName("Xhazam");
+    droneDto.setDroneStatus("disponivel");
+    
+    given().contentType(ContentType.JSON).body(droneDto).post("/drone");
+    
+    DroneDto updatedDrone = new DroneDto();
+    updatedDrone.setName("Master Blaster");
+    updatedDrone.setDroneStatus("indisponivel");
+
+    given()
+    .contentType(ContentType.JSON)
+    .body(updatedDrone)
+    .patch("/drone/1")
+    .then()
+      .statusCode(200)
+      .body("name", is("Master Blaster"));
+  }
+  
+  @Test
+  @DisplayName("5 - Deletar drone")
+  public void deveDeletarDrone() {
+    given().delete("/drone/1");
+    given().when().get("/drone").then().statusCode(200).body("list.size()", is(1));
   }
 }
